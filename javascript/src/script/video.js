961e878032f7ce7c642aa3abca436a590137c44b
Miris HP <miriamhdezsc9@gmail.com>
2020-07-08 23:13:53 -0500

    se reorganizo las carpetas

--- javascript/src/script/video.js
+++
@@ -1,7 +0,0 @@
-import fnMediaPlayer from './mediaPlayer.js';
-
-const video=document.querySelector("video"),
-			button= document.querySelector("button");
-
-const player = new fnMediaPlayer({el:video});
-button.onclick =()=>player.togglePay();
